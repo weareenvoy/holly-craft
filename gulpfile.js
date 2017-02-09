b244dd98513c5f920f5c55bad78a36326d25c5b9
Keenan <keenan@weareenvoy.com>
2017-02-08 17:27:21 -0800

    update gulpfile with baseConfig

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
+/**
+  - - - - -
+  H O L L Y
+  - - - - -
+  ./gulpfile.js
+  ENVOY frontend Gulp build system and boilerplate for Craft 3.
+  @author ENVOY development team
+ */
+
+global.baseConfig = require('./node_modules/holly/config')
 global.config = require('./config');
 require('./node_modules/holly/buildsys');
 require('./buildsys')
--- templates/gulpfile.tpl.js
+++ templates/gulpfile.tpl.js
@@ -3,10 +3,11 @@
   H O L L Y
   - - - - -
   ./gulpfile.js
-  ENVOY frontend Gulp build system and boilerplate for Craft 3 CMS.
+  ENVOY frontend Gulp build system and boilerplate for Craft 3.
   @author ENVOY development team
  */
 
+global.baseConfig = require('./node_modules/holly/config')
 global.config = require('./node_modules/holly-craft/config')
 require('./node_modules/holly/buildsys')
 require('./node_modules/holly-craft/buildsys')
