ffee70f6febc27479b14c9f531ff49c8f6e60b10
Keenan <keenan@weareenvoy.com>
2017-10-19 10:34:00 -0700

    updating envoy-holly and restructuring configs

--- buildsys/default-tasks.js
+++
@@ -1,7 +0,0 @@
-/**
- * ./buildsys/default-tasks.js
- * Gulp task series commands are defined here.
- * @author Keenan Staffieri
- */
-
-module.exports = []
--- install.js
+++ install.js
@@ -1,5 +1,4 @@
 var fs = require('fs')
-
 var prjPath = '../..'
 
 // Scaffold initial gulpfile
