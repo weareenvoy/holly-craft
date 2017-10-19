b5a8d26c7db9c5c20193b55a48d02af2fde0da31
Keenan <keenan@weareenvoy.com>
2017-10-19 14:23:10 -0700

    updating envoy-holly

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,19 @@
   H O L L Y
   - - - - -
   ENVOYsites frontend Gulp build system and boilerplate.
+  ** Craft 3 CMS Edition **
   @author ENVOY web development team
  */
 
 global.config = require('./node_modules/envoy-holly/config')
+
+// Craft-specific config overrides
+var distRoot = './web/dist'
+config.buildName = 'holly-craft'
+config.runStandalone = false
+config.paths.distRoot = distRoot
+config.assets.paths.output = distRoot + '/assets'
+config.assets.paths.styles = distRoot + '/css'
+config.assets.paths.scripts = distRoot + '/js'
+
 require('./node_modules/envoy-holly/buildsys')
